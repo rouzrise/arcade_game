9e0c89e8eb552da6b0c963e3c5d151b7b4965483
Rouzrise <rouzrise@gmail.com>
2018-04-25 10:50:54 +0300

    Initial commit
    Add basic functionality - Bugs run and Player move (Bugs and Player
    classes created). Function to check collisions is realized.
    At the moment of colliding there is a red splash and the Player is
    inverted (inverted image added) and gets to the grass at random square.
    At the moment of reaching Water the Player gets to the grass at
    random square.
    Some base for future game control added to HTML (game over msg, score,
    play again button).

---
+++ js/app.js
@@ -0,0 +1,143 @@
+
+var playerSpeed = 50;
+// var enemySpeed = 100; 
+// optionally - find the way to set enemySpeed through changing earlier defined speed
+var allEnemies = [];
+var gameTime = 0;
+var isGameOver;
+var score = 0;
+var scoreEl = document.getElementById('score');
+function getRandomArbitary(min, max) {
+    return Math.random() * (max - min) + min;
+};
+function randomInteger(min, max) {
+    var rand = min + Math.random() * (max + 1 - min);
+    rand = Math.floor(rand);
+    return rand;
+  }
+//step - 101
+var xOptions = [1, 102, 203, 304, 405];
+//step - 83
+var yOptions = [400, 317, 234, 151, 68, -15];
+
+
+///TODO - Set generic class "Entity" so that Enemy, Player, future Gems could inherit from it
+
+///ENEMY
+
+var Enemy = function(speed, y) {
+    this.sprite = 'images/enemy-bug.png';
+    this.x = 0;
+    this.y = y;
+    this.speed = speed;
+};
+
+Enemy.prototype.update = function(dt) {
+
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    this.x = this.x + this.speed * dt;
+    if(this.x > canvas.width) {
+        this.x = getRandomArbitary(-1000, -50);
+        }
+scoreEl.innerHTML = score;
+    };
+
+
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+var Enemies = function () {
+    this.preliminaryEnemiesArray = [];
+}
+
+
+
+
+
+Enemies.prototype.create = function (num) {
+   
+
+    for(var i = 0; i < num; i++) {
+        // debugger
+        var speed = getRandomArbitary(50, 400);
+       
+        var position = randomInteger(2, 4);
+      
+        this.preliminaryEnemiesArray[allEnemies.length] = new Enemy(speed, yOptions[position]);
+       allEnemies.push(this.preliminaryEnemiesArray[allEnemies.length]);
+      
+        
+    }
+}
+
+Enemies.prototype.reset = function() {
+    allEnemies = [];
+};
+
+var enemies = new Enemies();
+
+enemies.create(5);
+
+
+//// PLAYER
+var Player = function() {
+    this.sprite = 'images/char-cat-girl.png';
+    this.x = 203;
+    this.y =  400;
+};
+
+Player.prototype.update = function() {
+    this.xNow = this.x;
+    this.yNow = this.y;
+};
+
+Player.prototype.reset = function() {
+    this.x = 203;
+    this.y = 400;
+};
+
+Player.prototype.returnToStart = function() {
+
+    var position = randomInteger(0, 4);
+    this.x = xOptions[position];
+    this.y = 400;
+};
+
+Player.prototype.handleInput = function (pressedKey) {
+    if (pressedKey === 'left' && this.x >= 101) {
+     this.x -= 101;
+    }
+    if (pressedKey === 'up' && this.y >= 68) {
+        this.y -= 83;
+    }
+    if (pressedKey === 'right' && this.x <= 304) {
+        this.x += 101;
+    }
+    if (pressedKey === 'down' && this.y <= 317) {
+      this.y += 83;
+    }
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+var player = new Player();
+
+//create Gems
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keydown', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
